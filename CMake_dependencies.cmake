f43767f0aafb02753b0994192720eec7d1c977d6
Michal Lurie <Michal.Lurie@gmail.com>
2025-07-08 15:15:15 +0200

    Dep: xxhash cmake SOURCE_SUBDIR fix

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -12,6 +12,7 @@ FetchContent_Declare(
     xxhash
     GIT_REPOSITORY https://github.com/Cyan4973/xxHash.git
     GIT_TAG v0.8.3
+    SOURCE_SUBDIR cmake_unofficial
 )
 FetchContent_MakeAvailable(xxhash)
 
