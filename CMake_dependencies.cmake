b50c5fae0f55a6a5fc85a359e5154ad12cb3b39c
Michal Lurie <Michal.Lurie@gmail.com>
2025-11-03 16:10:03 +0100

    CMake: Integrated CPM

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,25 +1,8 @@
-include(FetchContent)
+include(cmake/CPM.cmake)
 
-FetchContent_Declare(
-    more_concepts
-    GIT_REPOSITORY https://github.com/InCom-0/more_concepts.git
-    GIT_TAG origin/master
-)
-FetchContent_MakeAvailable(more_concepts)
-
-
-FetchContent_Declare(
-    xxhash
-    GIT_REPOSITORY https://github.com/Cyan4973/xxHash.git
-    GIT_TAG v0.8.3
+CPMAddPackage("gh:InCom-0/more_concepts#master")
+CPMAddPackage(
+    URI "gh:Cyan4973/xxHash@0.8.3"
     SOURCE_SUBDIR cmake_unofficial
 )
-FetchContent_MakeAvailable(xxhash)
-
-
-FetchContent_Declare(
-    unordered_dense
-    GIT_REPOSITORY https://github.com/martinus/unordered_dense.git
-    GIT_TAG v4.5.0
-)
-FetchContent_MakeAvailable(unordered_dense)
+CPMAddPackage("gh:martinus/unordered_dense@4.5.0")
