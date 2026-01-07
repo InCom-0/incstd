337105aff846357eaf10bb4508f88db5404edafc
Michal Lurie <Michal.Lurie@gmail.com>
2026-01-07 21:18:16 +0100

    CMake: Getting all things CMake up to date

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,8 +1,15 @@
 include(cmake/CPM.cmake)
 
+if(BUILD_SHARED_LIBS)
+    set(CPM_USE_LOCAL_PACKAGES ON)
+endif()
+
 CPMAddPackage("gh:MiSo1289/more_concepts#master")
 CPMAddPackage(
     URI "gh:Cyan4973/xxHash@0.8.3"
     SOURCE_SUBDIR cmake_unofficial
+    NAME xxHash
 )
-CPMAddPackage("gh:martinus/unordered_dense@4.5.0")
+CPMAddPackage(
+    URI "gh:martinus/unordered_dense@4.5.0"
+    NAME unordered_dense)
