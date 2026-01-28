b7e31ea372276042e437a8ac3372a98f093bbefb
Michal Lurie <Michal.Lurie@gmail.com>
2026-01-28 14:16:39 +0100

    Feat: Finding xxHash using PkgConfig

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,18 +1,27 @@
 include(cmake/CPM.cmake)
 
-if(BUILD_SHARED_LIBS)
+if(NOT CPM_USE_LOCAL_PACKAGES)
     set(CPM_USE_LOCAL_PACKAGES ON)
 endif()
 
 CPMAddPackage("gh:MiSo1289/more_concepts#master")
 
-set(XXHASH_BUILD_XXHSUM OFF)
-CPMAddPackage(
-    URI "gh:Cyan4973/xxHash@0.8.3"
-    SOURCE_SUBDIR cmake_unofficial
-    OPTIONS "BUILD_SHARED_LIBS OFF"
-    NAME xxHash
-)
+
+find_package(xxHash QUIET)
+if(NOT xxHash_FOUND)
+    # Ensure CMake can find your FindxxHash.cmake
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
+    find_package(xxHash QUIET)
+endif()
+
+if(NOT xxHash_FOUND)
+    CPMAddPackage(
+        URI "gh:Cyan4973/xxHash@0.8.3"
+        SOURCE_SUBDIR cmake_unofficial
+        OPTIONS "BUILD_SHARED_LIBS OFF" "XXHASH_BUILD_XXHSUM OFF"
+        NAME xxHash
+    )
+endif()
 
 CPMAddPackage(
     URI "gh:martinus/unordered_dense@4.5.0"
---
+++ cmake/modules/FindxxHash.cmake
@@ -0,0 +1,25 @@
+find_package(PkgConfig QUIET)
+
+if(PkgConfig_FOUND)
+    pkg_check_modules(XXHASH QUIET IMPORTED_TARGET libxxhash)
+endif()
+
+# Fallback if the IMPORTED target wasn't created
+if(XXHASH_FOUND AND NOT TARGET PkgConfig::XXHASH)
+    add_library(PkgConfig::XXHASH INTERFACE IMPORTED)
+    target_include_directories(PkgConfig::XXHASH INTERFACE ${XXHASH_INCLUDE_DIRS})
+    target_link_libraries(PkgConfig::XXHASH INTERFACE ${XXHASH_LIBRARIES})
+endif()
+
+if(TARGET PkgConfig::XXHASH)
+    add_library(xxHash::xxhash ALIAS PkgConfig::XXHASH)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(xxHash DEFAULT_MSG
+    XXHASH_FOUND
+    XXHASH_LIBRARIES
+    XXHASH_INCLUDE_DIRS
+)
+
+set(xxHash_FOUND ${xxHash_FOUND})
