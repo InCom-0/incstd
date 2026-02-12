39306caac21e5a20a8b0aca4623dfbea27371bc7
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-12 18:58:02 +0100

    CMake: Try to update how we get xxhash pt.2

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -12,19 +12,19 @@ include(cmake/CPM.cmake)
 CPMAddPackage("gh:MiSo1289/more_concepts#master")
 
 # Try regular find
-find_package(xxHash QUIET)
+# find_package(xxHash QUIET)
 
 # If not inject local script that internally uses PkgConfig
-if(NOT xxHash_FOUND)
-    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
-endif()
+# if(NOT xxHash_FOUND)
+#     list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
+# endif()
 
 # Try again with CPM, if not found either then build from source
 CPMAddPackage(
     URI "gh:Cyan4973/xxHash#dev"
     SOURCE_SUBDIR build/cmake
     OPTIONS "BUILD_SHARED_LIBS OFF" "XXHASH_BUILD_XXHSUM OFF"
-    NAME xxhash
+    NAME xxHash
 )
 
 CPMAddPackage(
