0ea6fa861cd46fff8f400f7a6e9b3cead403c609
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-27 13:39:26 +0100

    Feat: AnsiToHtml also added foreground color defaulting

--- incl/incstd/console/ansi2html.hpp
+++ incl/incstd/console/ansi2html.hpp
@@ -210,6 +210,7 @@ public:
                     out.append("overflow-y: hidden;\n"sv);
                     out.append("max-width: 100%;\n"sv);
                     out.append("display: inline-block;\n"sv);
+                    out.append(std::string("color: "sv).append(opts_.schm.foreground.to_hex()).append(";\n"));
                     out.append(std::string("background-color: "sv).append(opts_.schm.backgrond.to_hex()).append(";\n"));
                 }
 
@@ -275,6 +276,7 @@ public:
                     out.append("overflow-y: hidden;\n"sv);
                     out.append("max-width: 100%;\n"sv);
                     out.append("display: inline-block;\n"sv);
+                    out.append(std::string("color: "sv).append(opts_.schm.foreground.to_hex()).append(";\n"));
                     out.append(std::string("background-color: "sv).append(opts_.schm.backgrond.to_hex()).append(";\n"));
                 }
 
