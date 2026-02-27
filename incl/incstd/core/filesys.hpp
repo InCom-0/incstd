7e8755cc481536acc7321a1a1a502ab54652e9d7
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-27 09:34:24 +0100

    Doc: added some todos

--- incl/incstd/core/filesys.hpp
+++ incl/incstd/core/filesys.hpp
@@ -31,6 +31,8 @@
 namespace incom::standard::filesys {
 namespace fs = std::filesystem;
 
+// TODO: This is somewhat weird, probably not at all smart to do it this way
+// TODO: At any rate change the interface so that it is using std::expected
 inline std::optional<std::string_view> get_file_textual(std::string_view const &sv) {
     static ankerl::unordered_dense::map<std::string, std::string, hashing::XXH3Hasher> storageMP;
     if (auto ele = storageMP.find(std::string(sv)); ele != storageMP.end()) { return std::string_view(ele->second); }
@@ -39,13 +41,14 @@ inline std::optional<std::string_view> get_file_textual(std::string_view const &
         ifs.open(fs::path(sv));
         if (ifs.is_open()) {
             return storageMP
-                .insert({std::string(sv), std::string(std::istreambuf_iterator(ifs), std::istreambuf_iterator<char>())})
-                .first->second;
+            .insert({std::string(sv), std::string(std::istreambuf_iterator(ifs), std::istreambuf_iterator<char>())})
+            .first->second;
         }
         else { return std::nullopt; }
     };
 }
 
+// TODO: At any rate change the interface so that it is using std::expected
 inline std::optional<std::vector<std::byte>> get_file_bytes(std::string_view const &sv) {
     std::ifstream ifs;
     ifs.open(fs::path(sv));
