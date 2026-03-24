7dab9442b885986169f6c7a5a93f6dd4620cfca6
Michal Lurie <Michal.Lurie@gmail.com>
2026-03-25 00:59:10 +0100

    Doc: Removed old comments

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -2,23 +2,13 @@ include(cmake/lefticus/CPM.cmake)
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/incom/modules")
 
 
-
 CPMAddPackage("gh:MiSo1289/more_concepts#master")
-
-# Try regular find
-# find_package(xxHash QUIET)
-
-# If not inject local script that internally uses PkgConfig
-# if(NOT xxHash_FOUND)
-#     list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
-# endif()
 
 # Try again with CPM, if not found either then build from source
 CPMAddPackage(
     URI "gh:Cyan4973/xxHash#dev"
     SOURCE_SUBDIR build/cmake
     OPTIONS "BUILD_SHARED_LIBS OFF" "XXHASH_BUILD_XXHSUM OFF"
-    # FORCE TRUE
     NAME xxHash
 )
 
